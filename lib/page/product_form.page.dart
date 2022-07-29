95aa7112c9d70708fae29c4f57c3529b568f5297
Gabriel Arsenio <gabrielarsenioo@gmail.com>
2022-07-29 18:26:11 +0100

    Fixed Products CRUD navigation

--- lib/page/product_form.page.dart
+++ lib/page/product_form.page.dart
@@ -3,11 +3,48 @@ import 'package:flutter/material.dart';
 class ProductFormPage extends StatelessWidget {
   const ProductFormPage({Key? key}) : super(key: key);
 
+  _textField(
+      {required String labelText, onChanged, String Function()? errorText}) {
+    return TextField(
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        labelText: labelText,
+        errorText: errorText == null ? null : errorText(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Produtos"),
+        title: const Text("Produto"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 25, left: 20, right: 20),
+        child: Column(
+          children: [
+            _textField(labelText: "Nome"),
+            const SizedBox(height: 20),
+            _textField(labelText: "Preço"),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: () {}, child: const Text("Salvar")),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(primary: Colors.red),
+                  child: const Text("Cancelar"),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/page/product_list.page.dart
+++ lib/page/product_list.page.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:pedidex_flutter/page/product_form.page.dart';
 
 class ProductListPage extends StatelessWidget {
   const ProductListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 5,
-      itemBuilder: (context, index) {
-        return ListTile(
-            title: Text("Produto 1"),
-            trailing: IconButton(
-              color: Colors.red,
-              icon: const Icon(Icons.remove_circle),
-              onPressed: () {},
-            ));
-      },
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Produtos"),
+        ),
+        body: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return ListTile(
+                title: Text("Produto 1"),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const ProductFormPage()));
+                });
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const ProductFormPage()));
+          },
+          tooltip: 'Novo produto',
+          child: const Icon(Icons.add),
+        ));
   }
 }
